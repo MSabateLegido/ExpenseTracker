f6ab687e259c8d7143b67b91df1a0610d5ebc4c1
Marc Sabaté Legido <64907031+MSabateLegido@users.noreply.github.com>
2026-01-23 01:09:51 +0100

    Added scrollState

--- app/src/main/java/com/example/expensetracker/presentation/categories/AddCategoryScreen.kt
+++ app/src/main/java/com/example/expensetracker/presentation/categories/AddCategoryScreen.kt
@@ -17,10 +17,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
@@ -80,11 +82,14 @@ fun AddCategoryScreen(
         }
     ) { paddingValues ->
 
+        val scrollState = rememberScrollState()
+        
         Column(
             modifier = Modifier
                 .padding(paddingValues)
                 .padding(16.dp)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(scrollState),
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
 
